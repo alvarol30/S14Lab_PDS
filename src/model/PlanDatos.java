7b82ac3504f328c5d441d3ad624662dc80f364db
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-12 15:08:13 -0500

    feat: Implementar clase abstracta PlanDatos con sus metodos y atributos respectivos

--- src/model/PlanDatos.java
+++ src/model/PlanDatos.java
@@ -1,4 +1,17 @@
 package model;
 
-public abstract class PlanDatos {
+public abstract class PlanDatos{
+    private String nombre;
+    private float tasa;
+
+    public void setTasa(float tasa) {
+        this.tasa = tasa;
+    }
+
+    public abstract float calculoPago(int datos);
+
+    public String toString(){
+        return "Nombre: "+nombre+
+                "\nTasa: "+tasa;
+    }
 }
