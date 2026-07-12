9c43225a31fd3e30761a723c3917a7ed34ea9a83
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-12 16:35:33 -0500

    feat: Implementar la clase hija FabricaLineaRapida con 3 tipos de planes contenidos en un switch

--- src/factory/FabricaLineaRapida.java
+++ src/factory/FabricaLineaRapida.java
@@ -1,4 +1,32 @@
 package factory;
 
-public class FabricaLineaRapida {
+import model.PlanDatos;
+import model.PlanDatosAcademico;
+import model.PlanDatosNegocio;
+import model.PlanDatosPersonal;
+
+public class FabricaLineaRapida extends FabricaCreadora{
+
+    @Override
+    public PlanDatos crearPlanDatos(String tipo){
+        PlanDatos plan;
+
+        switch (tipo.toLowerCase()){
+            case "academico":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.0f);
+                break;
+            case "personal":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(0.5f);
+                break;
+            case "negocio":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.5f);
+                break;
+            default:
+                throw new IllegalArgumentException("Tipo de plan inválido");
+        }
+        return plan;
+    }
 }
