977d08271f9764584badf56e99d1f9245d3a478b
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-12 16:39:25 -0500

    feat: Implementar la clases hijas restantes de FabricaCreadora, mismo codigo pero diferente valor de tasa por plan

--- src/factory/FabricaLineaClaro.java
+++ src/factory/FabricaLineaClaro.java
@@ -1,4 +1,32 @@
 package factory;
 
-public class FabricaLineaClaro {
+import model.PlanDatos;
+import model.PlanDatosAcademico;
+import model.PlanDatosNegocio;
+import model.PlanDatosPersonal;
+
+public class FabricaLineaClaro extends FabricaCreadora{
+
+    @Override
+    public PlanDatos crearPlanDatos(String tipo){
+        PlanDatos plan;
+
+        switch (tipo.toLowerCase()){
+            case "academico":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.2f);
+                break;
+            case "personal":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(0.6f);
+                break;
+            case "negocio":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.6f);
+                break;
+            default:
+                throw new IllegalArgumentException("Tipo de plan inválido");
+        }
+        return plan;
+    }
 }
--- src/factory/FabricaLineaMovistar.java
+++ src/factory/FabricaLineaMovistar.java
@@ -1,4 +1,32 @@
 package factory;
 
-public class FabricaLineaMovistar{
+import model.PlanDatos;
+import model.PlanDatosAcademico;
+import model.PlanDatosNegocio;
+import model.PlanDatosPersonal;
+
+public class FabricaLineaMovistar extends FabricaCreadora{
+
+    @Override
+    public PlanDatos crearPlanDatos(String tipo){
+        PlanDatos plan;
+
+        switch (tipo.toLowerCase()){
+            case "academico":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.25f);
+                break;
+            case "personal":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(0.8f);
+                break;
+            case "negocio":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.6f);
+                break;
+            default:
+                throw new IllegalArgumentException("Tipo de plan inválido");
+        }
+        return plan;
+    }
 }
